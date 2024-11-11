d1fa48f719c0edbe6448f45b731ed09a81a6cb2c
laurent <lhutinet@free.fr>
2024-11-11 12:42:02 +0100

    Add files via upload

--- calculatrice/index.js
+++ calculatrice/index.js
@@ -4,19 +4,30 @@
 
 // test
 // window.addEventListener("click", (e) => {
-//   console.log(e.target);
+//   console.log(e);
 // });
 // partie a supprimée
 
 let result = document.querySelector(".resultat").textContent;
-const btn =document.querySelectorAll("button");
+let preResult = "";
+const btn = document.querySelectorAll("button");
+const enter = document.getElementById("enter");
+// const
 
+btn.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const texteBoutonClique = event.target.textContent;
 
-document.addEventListener("click", () => {
- 
-  console.log(btn.textContent);
+    preResult += texteBoutonClique;
+    let resultFinal = "";
+
+    resultFinal = eval(preResult);
+    result.textContent = preResult;
+    enter.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      document.querySelector(".resultat").textContent = resultFinal;
+      resultFinal = "";
+    });
+  });
 });
-
-
-result = "tut";
-document.querySelector(".resultat").textContent=result
